05051adadbf70bfe648f09afdcfccda8742891c9
oumaru894 <bahoumar654321@gmail.com>
2025-07-16 19:08:50 +0000

    remove .env files

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,13 +6,14 @@ loadEnv(process.env.NODE_ENV || 'production', process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    database_type: "postgres",
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    }
+    },
   },
 
   plugins: [
