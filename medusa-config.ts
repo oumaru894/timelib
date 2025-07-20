3da26d3dd6740badf675053aacd760b4cac54275
oumaru894 <bahoumar654321@gmail.com>
2025-07-20 14:28:17 +0000

    admin serve set to false

--- medusa-config.ts
+++ medusa-config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
     admin: {
-      serve: true,
+      serve: false,
     },
     http: {
       storeCors: process.env.STORE_CORS!,
