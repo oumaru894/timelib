ebd57f06edc8df785e13c460e5e796475cdb8b8f
oumaru894 <bahoumar654321@gmail.com>
2025-07-28 00:04:50 +0000

    configure medusa-config.ts for production

--- medusa-config.ts
+++ medusa-config.ts
@@ -6,19 +6,38 @@ loadEnv(process.env.NODE_ENV || 'production', process.cwd())
 export default defineConfig({
   projectConfig: {
     databaseUrl: process.env.DATABASE_URL,
+    databaseDriverOptions: process.env.NODE_ENV !== "development" ?
+      { connection: { ssl: { rejectUnauthorized: false } } } : {},
+    workerMode: process.env.MEDUSA_WORKER_MODE as "shared" | "worker" | "server",
+    redisUrl: process.env.REDIS_URL,
+    /* cookieOptions: {
+      secure: "boolean",
+    }, */
+
     
     http: {
       storeCors: process.env.STORE_CORS!,
       adminCors: process.env.ADMIN_CORS!,
       authCors: process.env.AUTH_CORS!,
-      jwtSecret: process.env.JWT_SECRET || "supersecret",
-      cookieSecret: process.env.COOKIE_SECRET || "supersecret",
-    },
+      jwtSecret: process.env.JWT_SECRET,
+      jwtExpiresIn: process.env.JWT_EXPIRES_IN || "30d",
+      cookieSecret: process.env.COOKIE_SECRET,
+      authMethodsPerActor: {
+        user: ["emailpass"],
+        customer: ["emailpass", "google"],
+      },
     
+    },
   },
   
-
-  
+  modules: [
+    {
+      resolve: "@medusajs/medusa/cache-redis",
+      options: { 
+        redisUrl: process.env.CACHE_REDIS_URL,
+      },
+    },
+  ],
 
  // ...existing code...
 
