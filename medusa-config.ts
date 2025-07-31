94e3ec8cfb29dd9d2aa7646318f1cc4c6cf84504
oumaru894 <bahoumar654321@gmail.com>
2025-07-31 18:18:09 +0000

    fix minio config

--- medusa-config.ts
+++ medusa-config.ts
@@ -51,12 +51,12 @@ export default defineConfig({
             resolve: "@medusajs/medusa/file-s3",
             id: "s3",
             options: {
-              file_url: process.env.S3_FILE_URL,
-              access_key_id: process.env.S3_ACCESS_KEY_ID,
-              secret_access_key: process.env.S3_SECRET_ACCESS_KEY,
-              region: process.env.S3_REGION,
-              bucket: process.env.S3_BUCKET,
-              endpoint: process.env.S3_ENDPOINT,
+              file_url: process.env.MINIO_FILE_URL,
+              access_key_id: process.env.MINIO_ACCESS_KEY,
+              secret_access_key: process.env.MINIO_SECRET_KEY,
+              region: process.env.MINIO_REGION,
+              bucket: process.env.MINIO_BUCKET,
+              endpoint: process.env.MINIO_ENDPOINT,
               additional_client_config: {
                 forcePathStyle: true,
 
