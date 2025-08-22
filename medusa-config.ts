ae72d30997af07254ff4d04193e42c86f3d06726
oumaru894 <bahoumar654321@gmail.com>
2025-08-22 05:18:36 +0000

    added meilisearch

--- medusa-config.ts
+++ medusa-config.ts
@@ -64,5 +64,27 @@ export default defineConfig({
         ],
       },
     },
+    {
+    resolve: '@rokmohar/medusa-plugin-meilisearch',
+    options: {
+      config: {
+        host: process.env.MEILISEARCH_HOST,
+        apiKey: process.env.MEILISEARCH_API_KEY
+      },
+      settings: {
+          products: {
+            type: 'products',
+            enabled: true,
+            fields: ['id', 'title', 'description', 'handle', 'variant_sku', 'thumbnail'],
+            indexSettings: {
+              searchableAttributes: ['title', 'description', 'variant_sku'],
+              displayedAttributes: ['id', 'handle', 'title', 'description', 'variant_sku', 'thumbnail'],
+              filterableAttributes: ['id', 'handle'],
+            },
+            primaryKey: 'id',
+          }
+        }
+    }
+  }
   ],
 })
---
+++ src/subscribers/product-delete.ts
@@ -0,0 +1,14 @@
+import type { SubscriberArgs, SubscriberConfig } from '@medusajs/framework'
+import { ProductEvents } from '@medusajs/framework/utils'
+import { MeiliSearchService } from '@rokmohar/medusa-plugin-meilisearch'
+
+export default async function productDeleteHandler({ event: { data }, container }: SubscriberArgs<{ id: string }>) {
+    const productId = data.id
+
+    const meiliSearchService: MeiliSearchService = container.resolve('meilisearch')
+    await meiliSearchService.deleteDocument('products', productId)
+}
+
+export const config: SubscriberConfig = {
+    event: ProductEvents.PRODUCT_DELETED
+}
